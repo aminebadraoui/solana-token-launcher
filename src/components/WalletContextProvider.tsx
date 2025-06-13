ecab3f21ec4e99338193cc40db1b1893f5622597
Amine Badraoui <aminebadraoui94@gmail.com>
2025-06-13 19:57:06 -0400

    fix wallet adapters - remove USB dependency

--- src/components/WalletContextProvider.tsx
+++ src/components/WalletContextProvider.tsx
@@ -4,7 +4,9 @@ import React, { useMemo, useEffect, useState } from 'react';
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
 import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
 import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
-import { PhantomWalletAdapter, SolflareWalletAdapter } from '@solana/wallet-adapter-wallets';
+import { PhantomWalletAdapter } from '@solana/wallet-adapter-phantom';
+import { SolflareWalletAdapter } from '@solana/wallet-adapter-solflare';
+import { CoinbaseWalletAdapter } from '@solana/wallet-adapter-coinbase';
 import { clusterApiUrl } from '@solana/web3.js';
 
 // Import wallet adapter CSS
@@ -38,6 +40,7 @@ export function WalletContextProvider({ children }: { children: React.ReactNode 
         () => [
             new PhantomWalletAdapter(),
             new SolflareWalletAdapter(),
+            new CoinbaseWalletAdapter(),
         ],
         []
     );
